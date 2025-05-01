2f4e7daf2f3579a289bcab8cbe3b2ab888f832df
laboratorio <sirjaguar546@gmail.com>
2025-05-01 14:10:36 -0500

    update2

--- src/Heroe.java
+++ src/Heroe.java
@@ -4,4 +4,43 @@ public class Heroe {
     private String raza;
     private int nivelPoder;
     private String habilidad;
+
+    public Heroe(String nombre, String raza, int nivelPoder, String habilidad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.nivelPoder = nivelPoder;
+        this.habilidad = habilidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getNivelPoder() {
+        return nivelPoder;
+    }
+
+    public void setNivelPoder(int nivelPoder) {
+        this.nivelPoder = nivelPoder;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(String habilidad) {
+        this.habilidad = habilidad;
+    }
 }
